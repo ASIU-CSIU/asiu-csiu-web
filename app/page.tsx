ec07dc1acc3e83cff9eb23d4bcefc019c6d488ab
skyangel <skyangel@iu.edu>
2025-08-31 14:36:54 -0400

    fixed UI

--- app/page.tsx
+++ app/page.tsx
@@ -161,18 +161,23 @@ export default async function HomePage() {
                 Every contribution makes a difference in advancing science policy.
               </p>
               <div className="flex flex-col sm:flex-row gap-3 justify-center items-center">
-                <a
-                  href="https://www.gofundme.com/f/support-advocates-for-science-iu/donate?source=btn_donate"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="bg-white text-science-red px-6 py-2 rounded-lg font-bold hover:bg-gray-100 transition-colors duration-200 flex items-center"
+
+                <Button
+                  asChild
+                  className="bg-white text-science-red hover:bg-gray-100"
                 >
-                  <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
-                    <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
-                    <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd" />
-                  </svg>
-                  Donate Now
-                </a>
+                  <a
+                    href="https://www.gofundme.com/f/support-advocates-for-science-iu/donate?source=btn_donate"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                  >
+                    <svg className="w-4 h-4 mr-2" fill="currentColor" viewBox="0 0 20 20">
+                      <path d="M8.433 7.418c.155-.103.346-.196.567-.267v1.698a2.305 2.305 0 01-.567-.267C8.07 8.34 8 8.114 8 8c0-.114.07-.34.433-.582zM11 12.849v-1.698c.22.071.412.164.567.267.364.243.433.468.433.582 0 .114-.07.34-.433.582a2.305 2.305 0 01-.567.267z" />
+                      <path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm1-13a1 1 0 10-2 0v.092a4.535 4.535 0 00-1.676.662C6.602 6.234 6 7.009 6 8c0 .99.602 1.765 1.324 2.246.48.32 1.054.545 1.676.662v1.941c-.391-.127-.68-.317-.843-.504a1 1 0 10-1.51 1.31c.562.649 1.413 1.076 2.353 1.253V15a1 1 0 102 0v-.092a4.535 4.535 0 001.676-.662C13.398 13.766 14 12.991 14 12c0-.99-.602-1.765-1.324-2.246A4.535 4.535 0 0011 9.092V7.151c.391.127.68.317.843.504a1 1 0 101.511-1.31c-.563-.649-1.413-1.076-2.354-1.253V5z" clipRule="evenodd" />
+                    </svg>
+                    Donate Now
+                  </a>
+                </Button>
                 <div className="flex items-center text-white/80">
                   <span className="text-xs ml-1">Secure donations via</span>
                   <svg className="-mt-1.5 w-20 h-18" viewBox="0 0 652 652" fill="currentColor">
